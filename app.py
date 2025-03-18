4e528879dab61de42215350cbf72ff57d4833bb4
AlexM <AlexMonge14@outlook.com>
2025-03-18 19:48:56 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import tclab
+import time
+import threading
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+tclab_object = None
+tclab_connected = False
+thread_connection = None
+thread_update = None
+
+def check_tclab_connection():
+    global tclab_connected, tclab_object
+    while True:
+        try:
+            if not tclab_connected:
+                tclab_object = tclab.TCLab()
+                tclab_connected = True
+            temp = tclab_object.T1
+        except:
+            if tclab_connected:
+                tclab_connected = False
+                tclab_object = None
+
+        socketio.emit('tclab_status', {'connected': tclab_connected})
+        time.sleep(2)
+
+def set_heater(percentage, heater):
+    global tclab_object
+    try:
+        if heater == 1:
+            tclab_object.Q1(percentage)
+        else:
+            tclab_object.Q2(percentage)
+        tclab_object.LED(100)
+        time.sleep(1)
+        tclab_object.LED(20)
+        return True
+    except:
+        return False
+
+def update_data():
+    global tclab_object
+    while tclab_connected:
+        try:
+            q1 = tclab_object.Q1()
+            q2 = tclab_object.Q2()
+            t1 = tclab_object.T1
+            t2 = tclab_object.T2
+            print(q1, q2, t1, t2)
+            socketio.emit('tclab_data', {'Q1': q1, 'Q2': q2, 'T1': t1, 'T2': t2})
+            time.sleep(2)
+        except:
+            break
+            
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def handle_check_tclab_connection():
+    global thread_connection
+    if thread_connection is None:
+        thread_connection =  threading.Thread(target=check_tclab_connection, daemon=True)
+        thread_connection.start()
+    socketio.emit('server_status', {'connected': " E-TCLAB-W: Connection established successfully."})
+
+@socketio.on('set_heater')
+def handle_set_heater(data):
+    if set_heater(data['percentage'], data['heater']):
+        socketio.emit('heater_status', {'status': f"Heater {data['heater']} set to {data['percentage']}%"})
+
+@socketio.on('update_data')
+def handle_update_data():
+    global thread_update
+    if thread_update is None:
+        thread_update = threading.Thread(target=update_data, daemon=True)
+        thread_update.start()
+
+if __name__ == "__main__":
+    socketio.run(app, host='127.0.0.1', port=5000)
---
+++ static/script.js
@@ -0,0 +1,141 @@
+const socket = io.connect("http://127.0.0.1:5000");
+
+socket.on("tclab_status", (data) => {
+    const h1 = document.getElementById("response");
+    const statusText = data.connected ? "System Connected  :)" : "System Not found  :(";
+    const statusColor = data.connected ? "#00FF7F" : "red";
+
+    h1.innerHTML = statusText;
+    h1.style.color = statusColor;
+
+    document.querySelectorAll(".heater").forEach(button => button.disabled = !data.connected);
+
+    if (data.connected) socket.emit("update_data");
+});
+
+socket.on("server_status", (data) => {
+    logMessage(data.connected);
+});
+
+socket.on("heater_status", (data) => {
+    logMessage(data.status);
+});
+
+function logMessage(message) {
+    const consoleElement = document.getElementById("log");
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0'); 
+    const minutes = String(now.getMinutes()).padStart(2, '0'); 
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    const formattedMessage = `[${timeString}] ${message}`;
+    const newMessage = document.createElement("div");
+    newMessage.textContent = formattedMessage;
+    consoleElement.appendChild(newMessage);
+    consoleElement.scrollTop = consoleElement.scrollHeight;
+}
+
+function selectHeat(temp, dropdownId) {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let dropdown of dropdowns) {
+        dropdown.style.display = "none";
+    }
+    socket.emit('set_heater', {percentage: temp, heater: dropdownId});
+}
+
+function showDropdown(dropdownId) {
+    const dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = "block";
+}
+
+window.addEventListener('click', function(event) {
+    const dropdowns = document.getElementsByClassName("dropdown");
+    for (let dropdown of dropdowns) {
+        const dropdownButton = dropdown.querySelector('button');
+        const dropdownContent = dropdown.querySelector('.dropdown-content');
+        if (!dropdownButton.contains(event.target) && !dropdownContent.contains(event.target)) {
+            dropdownContent.style.display = "none";
+        }
+    }
+});
+
+const charts = {
+    Q1: createChart('Q1Chart', 'Q1 (Heater 1)', 'blue'),
+    Q2: createChart('Q2Chart', 'Q2 (Heater 2)', 'blue'),
+    T1: createChart('T1Chart', 'T1 (Temperature 1)', 'red'),
+    T2: createChart('T2Chart', 'T2 (Temperature 2)', 'red'),
+};
+
+start = 0;
+
+function createChart(elementId, label, borderColor) {
+    return new Chart(document.getElementById(elementId), {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{
+                label: label,
+                data: [],
+                borderColor: borderColor,
+                backgroundColor: borderColor,
+                fill: false,
+                tension: 0.1,
+                pointRadius: 5,
+            }]
+        },
+        options: {
+            responsive: false,
+            animation: false,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: '#567aeb',
+                        font: {
+                            size: 14
+                        }
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    type: 'linear',
+                    title: {
+                        display: true,
+                        text: 'Time',
+                        color: '#567aeb',
+                    },
+                    ticks: {
+                        color: 'white',
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: label.split(" ")[0],
+                        color: '#567aeb',
+                    },
+                    ticks: {
+                        color: 'white',
+                    }
+                }
+            }
+        }
+    });
+}
+
+function updateChart(chart, value, time) {
+    if(chart.data.datasets[0].data.length === 0)
+        start = time
+    chart.data.labels.push((time - start) / 1000);
+    chart.data.datasets[0].data.push(value);
+    chart.update();
+}
+
+socket.on("tclab_data", (data) => {
+    const time = new Date().getTime();
+    console.log(data);
+    updateChart(charts.Q1, data.Q1, time);
+    updateChart(charts.Q2, data.Q2, time);
+    updateChart(charts.T1, data.T1, time);
+    updateChart(charts.T2, data.T2, time);
+});
